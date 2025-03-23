a14acef65aa189f6012207798d193760b12fccbb
namdev <namnkq2603@gmail.com>
2025-03-23 13:40:53 +0700

    chore: add figma style guide by tailwindcss v3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+const HomePage = () => {
+  return (
+    <div>
+      <div className='text-body-small-400'>Body/Small/400</div>
+      <div className='text-body-small-500'>Body/Small/500</div>
+      <div className='text-body-small-600'>Body/Small/600</div>
+    </div>
+  )
 }
+
+export default HomePage
